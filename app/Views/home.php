b79c71c71abd93405748b371fc2ac7ffd76cd220
hpaanonlineuniversity <hpaanonlineuniversity@gmail.com>
2026-08-16 17:16:06 +0630

    update home controller

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html>
-<head><title><?= $title ?></title></head>
+<head>
+    <title><?= $title ?></title>
+</head>
 <body>
     <h1>User List</h1>
-    <ul>
-        <?php foreach ($users as $user): ?>
-            <li><?= htmlspecialchars($user['name']) ?> - <?= htmlspecialchars($user['email']) ?></li>
-        <?php endforeach; ?>
-    </ul>
+    
+    <?php if (!empty($users)): ?>
+        <ul>
+            <?php foreach ($users as $user): ?>
+                <li><?= htmlspecialchars($user['name']) ?> - <?= htmlspecialchars($user['email']) ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php else: ?>
+        <p>No users found.</p>
+    <?php endif; ?>
 </body>
 </html>
